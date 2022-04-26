126ded5fc2906a6d0df928a00e0c04871df67030
FuzenPuzenMLA <fuzenpuzen.mla@gmail.com>
2022-04-26 22:05:46 +0300

    Add shake

---
+++ Assets/Scripts/DOTweenAnimation/Session/CameraShakeAnimation.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class CameraShakeAnimation : MonoBehaviour
+{
+    public void DoShake()
+    {
+        transform.DOShakePosition(0.5f,0.3f,15,0,false,true);
+    }
+}
